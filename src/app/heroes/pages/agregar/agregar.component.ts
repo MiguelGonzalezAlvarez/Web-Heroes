20cb5f6dbac3f7663171061350ad422460047fdb
MiguelMuros <miggonzalv@gmail.com>
2021-10-17 18:18:55 +0200

    feat: design add screen

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AgregarComponent implements OnInit {
+
+  publishers = [{ id: 'DC Comics', descripcion: 'DC - Comics' }, { id: 'Marvel Comics', descripcion: 'Marvel - Comics' }];
+  heroe: Heroe = {
+    superhero: '',
+    publisher: Publisher.DCComics,
+    alter_ego: '',
+    first_appearance: '',
+    characters: '',
+    altImage: ''
+  };
 
   constructor() { }
 
