a2d6d100e411dec10d984af1391543c9487fd202
MiguelMuros <miggonzalv@gmail.com>
2021-10-17 13:41:37 +0200

    feat: use mat list in heroe screen

--- src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -4,7 +4,7 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
-  styles: []
+  styles: [`mat-card{margin-top:1rem;}`]
 })
 export class HeroeTarjetaComponent implements OnInit {
 
