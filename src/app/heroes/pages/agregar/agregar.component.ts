d9bfa697095e62a3c8ed64bc0413aba53d10bedf
MiguelMuros <miggonzalv@gmail.com>
2021-10-17 19:59:20 +0200

    feat: create edit and delete features for add screen

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [`img { width:100%; border-radius:5px }`]
 })
 export class AgregarComponent implements OnInit {
 
@@ -20,16 +21,32 @@ export class AgregarComponent implements OnInit {
     altImage: ''
   };
 
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    if (this.router.url.includes('editar')) {
+      // En el caso de estar editando obtenemos el heroe del id que nos pasan en la url
+      this.route.params
+        .pipe(switchMap(({ id }) => this.heroesService.getHeroePorId(id)))
+        .subscribe((response) => this.heroe = response);
+    }
   }
 
   guardar(): void {
-    console.log(this.heroe);
-    if (this.heroe.superhero.trim().length) {
-      this.heroesService.agregarHeroe(this.heroe).subscribe(response => console.log(response));
+    if (this.heroe.id) {
+      // Si tenemos id de la base de datos es que estamos editando
+      this.heroesService.actualizarHeroe(this.heroe).subscribe(response => console.log(response));
     }
+    else {
+      // En caso de no tener id es que estamos creando un nuevo heroe
+      this.heroesService.agregarHeroe(this.heroe).subscribe(response => {
+        this.router.navigate(['/heroes/editar', response.id]);
+      });
+    }
+  }
+
+  borrar(): void {
+    this.heroesService.borrarHeroe(this.heroe.id!).subscribe(() => { this.router.navigate(['/heroes']) });
   }
 
 }
--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -2,12 +2,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
 })
 export class ImagenPipe implements PipeTransform {
 
   transform(heroe: Heroe): string {
-    return `assets/heroes/${heroe.id}.jpg`;
+    if (!heroe.id && !heroe.altImage) {
+      return 'assets/no-image.png';
+    } else if (heroe.altImage) {
+      return heroe.altImage;
+    } else {
+      return `assets/heroes/${heroe.id}.jpg`;
+    }
   }
 
 }
